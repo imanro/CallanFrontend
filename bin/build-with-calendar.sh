ad27c0836861b5451bff300e77521d8c59e2f800
manro <roman.denisov@gmail.com>
2018-11-20 18:16:24 +0200

    next build

---
+++ bin/build-with-calendar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+DIST_DIR="$SCRIPT_DIR/../dist";
+BUILD_DIR="$SCRIPT_DIR/../builds";
+
+echo "Type a revision number, followed by [ENTER]: ";
+read REV
+echo "Type an environment name, followed by [ENTER] (default \"production\"): ";
+read ENV
+
+if [ "$ENV" == "" ]; then
+    ENV="production";
+fi;
+
+cd $SCRIPT_DIR/../
+
+echo "Updating git version..."
+node git-version.js
+
+echo "Building for $ENV...";
+ng build --configuration=$ENV
+
+FILE_NAME="$BUILD_DIR/callan-$REV.tar.gz";
+echo "Creating $FILE_NAME"
+
+tar -czf $FILE_NAME -C $DIST_DIR .
+
+exit 0;
