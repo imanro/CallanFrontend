4e3788046d00a6d85b2425ad88b66d35ae3175ec
imanro <roman.denisov@gmail.com>
2019-02-24 02:09:50 +0200

    deploy script modified

--- bin/ssh-deploy.sh
+++ bin/ssh-deploy.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-PROJECT_NAME="callan"
-REMOTE_HOST="81.95.188.7"
-REMOTE_USER="$USER"
-REMOTE_DIR_PREFIX="/var/www/vhosts"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+CFG_DIR="$SCRIPT_DIR/../etc"
+LIBS_DIR="$SCRIPT_DIR/libs";
+
 DEPLOY_DIR_NAME="www";
 DEPLOY_DIR="$SCRIPT_DIR/../$DEPLOY_DIR_NAME";
 BUILD_DIR_NAME="builds";
 BUILD_DIR="$SCRIPT_DIR/../$BUILD_DIR_NAME";
 ARCHIVE_EXT=".tar.gz";
 
-# take needle archive and place unto target host
-# we need predef vars: $ remote machine dir path
-# $ remote host
-# we ask: dir name
+. $LIBS_DIR/shini.sh
+
+__shini_parsed ()
+{
+    if [ "$1" == "$ENV" ]; then
+	export $2="$3";
+    fi;
+}
 
 if [ ! -e "$DEPLOY_DIR" ]; then
     echo "The deploy dir $DEPLOY_DIR is not exists"
@@ -31,6 +34,15 @@ if [ "$ENV" == "" ]; then
     ENV="production";
 fi;
 
+
+    shini_parse "$CFG_DIR/ssh-deploy.ini";
+
+    if [ -z "$PROJECT_NAME" ]; then
+	echo "Wrong environment name given: its not defined in ssh-deploy.ini";
+	exit 1;
+    fi;
+
+
 ARCHIVE_FILE_NAME="$PROJECT_NAME-$ENV-$REV$ARCHIVE_EXT"
 ARCHIVE_FILE="$BUILD_DIR/$ARCHIVE_FILE_NAME";
 
@@ -39,12 +51,6 @@ if [ ! -e "$ARCHIVE_FILE" ]; then
     exit 1;
 fi;
 
-echo "Type a remote dir name, followed by [ENTER], started with $REMOTE_DIR_PREFIX (default $PROJECT_NAME): "
-read REMOTE_PROJ_DIR_NAME;
-
-if [ "$REMOTE_PROJ_DIR_NAME" == "" ]; then
-    REMOTE_PROJ_DIR_NAME="$PROJECT_NAME";
-fi;
 
 REMOTE_DIR=$REMOTE_DIR_PREFIX/$REMOTE_PROJ_DIR_NAME
 
